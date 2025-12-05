2cb6896cd732c35c2d5ff412ea5d55c0290ddc01
Cecelia <cak248@cornell.edu>
2025-12-05 01:47:02 +0000

    door opener logic

--- 1_secret_entrance/door_opener.py
+++ 1_secret_entrance/door_opener.py
@@ -3,14 +3,10 @@ doc_path = Path('document.txt')
 arrow_location = 50
 zero_count = 0
 for line in doc_path.read_text().splitlines():
-    #print(line)
-    #arrow_location 
     if line.startswith('L'):
-        print('hello')
+        arrow_location -= int(line[1:])
     if line.startswith('R'):
-        print('goodbye')
-#mod_location = running total mod 100 
-#if mod location == 0 increment counter 
-#else keep going 
-
-print((doc_path.read_text().splitlines())[0])
+        arrow_location += int(line[1:])
+    if arrow_location % 100 == 0:
+        zero_count += 1 
+print(zero_count)
